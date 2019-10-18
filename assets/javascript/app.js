05674de973928825858d9aa5371c3f711aa82e2f
Sebastian Arnavat <sebastianarnavat@Sebastians-MacBook-Pro.local>
2019-10-18 16:18:55 -0400

    Added questions and answers to js

---
+++ assets/javascript/app.js
@@ -0,0 +1,47 @@
+//Q & A
+let triviaQ = [{
+    question: "What is Rick's favorite attraction in Anatomy Park?",
+    answerOptions: ["The Bone Train", "Spleen Mountain", "Bladder Falls", "Pirates of the Pancreas"],
+    answer: 3
+},{
+    question: "What is the meaning of 'wubaluba dub dub?",
+    answerOptions: ["I'm drunk", "I'm in great pain", "I'm smarter than everyone", "My wife left me"],
+    answer: 1
+},{
+    question: "Who does Morty have a crush on?",
+    answerOptions: [Tammy, Vanessa, Jessica, Jennifer],
+    answer: 2
+},{
+    question: "Who voices both Rick and Morty?",
+    answerOptions: ["Dan Harmon", "Justin Roiland", "Chris Parnell", "Morgan Freeman" ],
+    answer: 1
+},{
+    question: "What is on a cob on Cob Planet?".,
+    answerOptions: ["birds", "molecules", "corn", "everything"],
+    answer: 3 
+},{
+    question: "What was Jerry's successful advertising pitch?",
+    answerOptions: ["Thirsty for Milk?", "Hungry for Apples?", "Addicted to lasagna?", "Craving for Muffins?"],
+    answer: 1
+},{
+    quesion: "Who comes to the rescue when Rick and Morty are performing on 'Planet Music' in the episode 'Get Schwifty'?",
+    answerOptions: ["Obama", "Bird Person", "Water-T", "Ice-T"],
+    answer: 3
+},{
+    question: "Which longtime family-friend does Beth shoot?",
+    answerOptions: ["Mr. Poopybutthole", "Squanchy", "Bird Person", "Mr. Meeseeks"],
+    answer: 0
+},{
+    question: "Who is the only one allowed to have Eyeholes?",
+    answerOptions: ["Rick", "Tiny Rick", "Eyehole Man", "Jerry"],
+    answer: 2
+},{ 
+    question: "Who won the Gear Wars?",
+    answerOptions: ["The Gearheads", "The Cromulons", "The Bird People", "Nobody cares"],
+    Answer: 3
+}]
+
+    
+        
+    
+
