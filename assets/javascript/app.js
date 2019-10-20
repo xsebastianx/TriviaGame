07fb552a75bcfc8c0aa6e7eb6a5a8e611a972952
Sebastian Arnavat <sebastianarnavat@Sebastians-MacBook-Pro.local>
2019-10-20 14:06:06 -0400

    Updated html and added questions to js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,47 +1,108 @@
-//Q & A
-let triviaQ = [{
+$(document).ready(function(){
+
+  $("#timer").hide();
+  $("#doneBtn").hide(); 
+  $("#startBtn").on("click", questions.startGame)
+})
+
+let questions = [
+  {
     question: "What is Rick's favorite attraction in Anatomy Park?",
-    answerOptions: ["The Bone Train", "Spleen Mountain", "Bladder Falls", "Pirates of the Pancreas"],
-    answer: 3
-},{
+    answers: [
+      { answer: "The Bone Train", value: false },
+      { answer: "Spleen Mountain", value: false },
+      { answer: "Bladder Falls", value: false },
+      { answer: "Pirates of the Pancreas", value: true }
+    ]
+  },
+  {
     question: "What is the meaning of 'wubaluba dub dub?",
-    answerOptions: ["I'm drunk", "I'm in great pain", "I'm smarter than everyone", "My wife left me"],
-    answer: 1
-},{
+    answers: [
+      { answer: "I'm drunk", value: false },
+      { answer: "I'm in great pain", value: true },
+      { answer: "I'm smarter than everyone", value: false },
+      { answer: "My wife left me", value: false }
+    ]
+  },
+  {
     question: "Who does Morty have a crush on?",
-    answerOptions: [Tammy, Vanessa, Jessica, Jennifer],
-    answer: 2
-},{
+    answers: [
+      { answer: "Tammy", value: false },
+      { answer: "Vanessa", value: false },
+      { answer: "Jessica", value: true },
+      { answer: "Jennifer", value: false }
+    ]
+  },
+  {
     question: "Who voices both Rick and Morty?",
-    answerOptions: ["Dan Harmon", "Justin Roiland", "Chris Parnell", "Morgan Freeman" ],
-    answer: 1
-},{
-    question: "What is on a cob on Cob Planet?".,
-    answerOptions: ["birds", "molecules", "corn", "everything"],
-    answer: 3 
-},{
+    answers: [
+      { answer: "Dan Harmon", value: false },
+      { answer: "Justin Roiland", value: true },
+      { answer: "Chris Parnell", value: false },
+      { answer: "Morgan Freeman", value: false }
+    ]
+  },
+  {
+    question: "What is on a cob on Cob Planet?",
+    answers: [
+      { answer: "birds", value: false },
+      { answer: "molecules", value: false },
+      { answer: "corn", value: false },
+      { answer: "everything", value: true }
+    ]
+  },
+  {
     question: "What was Jerry's successful advertising pitch?",
-    answerOptions: ["Thirsty for Milk?", "Hungry for Apples?", "Addicted to lasagna?", "Craving for Muffins?"],
-    answer: 1
-},{
-    quesion: "Who comes to the rescue when Rick and Morty are performing on 'Planet Music' in the episode 'Get Schwifty'?",
-    answerOptions: ["Obama", "Bird Person", "Water-T", "Ice-T"],
-    answer: 3
-},{
+    answers: [
+      { answer: "Thirsty for Milk?", value: false },
+      { answer: "Hungry for Apples?", value: true },
+      { answer: "Addicted to lasagna?", value: false },
+      { answer: "Craving for Muffins?", value: false }
+    ]
+  },
+  {
+    question: "Who comes to the rescue when Rick and Morty are performing on 'Planet Music' in the episode 'Get Schwifty'?",
+    answers: [
+      { answer: "Obama", value: false },
+      { answer: "Bird Person", value: false },
+      { answer: "Water-T", value: false },
+      { answer: "Ice-T", value: true }
+    ]
+  },
+  {
     question: "Which longtime family-friend does Beth shoot?",
-    answerOptions: ["Mr. Poopybutthole", "Squanchy", "Bird Person", "Mr. Meeseeks"],
-    answer: 0
-},{
+    answers: [
+      { answer: "Mr. Poopybutthole", value: true },
+      { answer: "Squanchy", value: false },
+      { answer: "Bird Person", value: false },
+      { answer: "Mr. Meeseeks", value: false }
+    ]
+  },
+  {
     question: "Who is the only one allowed to have Eyeholes?",
-    answerOptions: ["Rick", "Tiny Rick", "Eyehole Man", "Jerry"],
-    answer: 2
-},{ 
+    answers: [
+      { answer: "Rick", value: false },
+      { answer: "Tiny Rick", value: false },
+      { answer: "Eyehole Man", value: true },
+      { answer: "Jerry", value: false }
+    ]
+  },
+  {
     question: "Who won the Gear Wars?",
-    answerOptions: ["The Gearheads", "The Cromulons", "The Bird People", "Nobody cares"],
-    Answer: 3
-}]
+    answers: [
+      { answer: "The Gearheads", value: false },
+      { answer: "The Cromulons", value: false },
+      { answer: "The Bird People", value: false },
+      { answer: "Nobody cares", value: true }
+    ]
+  }
+];     
 
-    
-        
-    
+let correct = 0;
+let incorrect = 0;
+let unanswered = 0;
+let timer = 90;
 
+startGame: function(){
+
+}
